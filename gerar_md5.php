41d72cf818ad06b3b68e4f42dd14ef6656bc50d3
DiegoNascimento1 <diegosoaresnascimento1@gmail.com>
2022-02-22 08:39:53 -0300

    test: testando md5 com combinação

--- gerar_md5.php
+++ gerar_md5.php
@@ -1,1 +1,11 @@
 <?php
+require_once 'gerar_combinacao.php';
+$comb = Combinacao::gerar_maiusculas(10);
+print_r($comb);
+echo "   ->   ";
+print_r(md5($comb));
+echo '<br>';
+$comb = Combinacao::gerar_todos_caracteres(12);
+print_r($comb);
+echo "   ->   ";
+print_r(md5($comb));
